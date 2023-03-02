4eb6ce37bd3a4a8e9e339ab710df797cb9b74a85
baranngungor <baran.gungor@easycep.com>
2023-03-02 23:03:54 +0300

    userApp

--- lib/main.dart
+++ lib/main.dart
@@ -24,27 +24,6 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class SplashScreen extends StatefulWidget {
-  const SplashScreen({super.key});
-  @override
-  State<SplashScreen> createState() => _SplashScreenState();
-}
-
-class _SplashScreenState extends State<SplashScreen> {
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Image.asset(''),
-    );
-  }
-}
-
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
